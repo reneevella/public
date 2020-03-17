f52fd89899e2e9febdce8bd43f2888bb9fae651d
reneevella <reneevella@users.noreply.github.com>
2020-03-17 19:51:27 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -60,8 +60,6 @@ int main(void)
        // printf("%s\n", flag);
     }
 
-printf("Digitos: %i\nContarDigitos: %i\nRestosum: %i\nCardnumber: %li\nFlag: %s\n\n\n", digitos,contardigitos, restosum, cardnumber,flag);
-    printf("%s\n", flag);
 
     if ((strcmp(flag, "a") == 0) && digitos == 15) 
     {
@@ -85,6 +83,6 @@ printf("Digitos: %i\nContarDigitos: %i\nRestosum: %i\nCardnumber: %li\nFlag: %s\
        // printf("%s\n", flag);
     }
 
-    printf("Digitos: %i\nContarDigitos: %i\nRestosum: %i\nCardnumber: %li\nFlag: %s\n\n\n", digitos,contardigitos, restosum, cardnumber,flag);
+   // printf("Digitos: %i\nContarDigitos: %i\nRestosum: %i\nCardnumber: %li\nFlag: %s\n\n\n", digitos,contardigitos, restosum, cardnumber,flag);
     printf("%s\n", flag);
 }
