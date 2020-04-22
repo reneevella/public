f5b0d713d5184e786fdf870ccfbd136b0526769a
reneevella <reneevella@users.noreply.github.com>
2020-04-22 18:52:52 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -3,36 +3,36 @@
 #include <stdint.h>
 
 
-    //fread(data, size, number, intptr);
+//fread(data, size, number, intptr);
 
-    //0xff 0xd8 0xff
+//0xff 0xd8 0xff
 
-    //cada bloco tem 512 bytes
+//cada bloco tem 512 bytes
 
-    // data pode ser um buffer, vetor onde vc vai alocar os dados que está lendo
-    //size numero de bytes que cada elemento que vc vai ler - 512
-    //number numero de elemntos que vc quer(tenta) ler
-    //intprt *FILE é o arquivo que vc vai ler os dados from - memory card
+// data pode ser um buffer, vetor onde vc vai alocar os dados que está lendo
+//size numero de bytes que cada elemento que vc vai ler - 512
+//number numero de elemntos que vc quer(tenta) ler
+//intprt *FILE é o arquivo que vc vai ler os dados from - memory card
 
-    //(buffer[3] & 0xf0) == 0xe0
+//(buffer[3] & 0xf0) == 0xe0
 
-    //guardar o numero de arquivos que achei
+//guardar o numero de arquivos que achei
 
-    //sprinft(filename, "%03i.jpg", numero_do_arquivo);
+//sprinft(filename, "%03i.jpg", numero_do_arquivo);
 
-    //FILE *img = fopen(filename, "w"); // a partir daqui pode começar a gravar dados no arquivo usando fwrite
+//FILE *img = fopen(filename, "w"); // a partir daqui pode começar a gravar dados no arquivo usando fwrite
 
-    //fwrite(data, size, number, outptr);
+//fwrite(data, size, number, outptr);
 
-    //data: poiter para os bytes que vão ser gravados no aruqivo
-    //size: tamanho de cada elemento a ser gravado
-    //number:  numero de elementos
-    //outptr: FILE * é o ponteiro para o arquivo que vc quer gravar os dados -- o jpg que vc acabou de abrir
+//data: poiter para os bytes que vão ser gravados no aruqivo
+//size: tamanho de cada elemento a ser gravado
+//number:  numero de elementos
+//outptr: FILE * é o ponteiro para o arquivo que vc quer gravar os dados -- o jpg que vc acabou de abrir
 
 
-    //fread retorna o numero de itens do tamanho size que foi lido do arquivo
-    //tenta ler n elentos, se não tiver n elentos ele retorna um valor menos que n
-    //criar condição para determinar se o fread chegou até o final do arquivo
+//fread retorna o numero de itens do tamanho size que foi lido do arquivo
+//tenta ler n elentos, se não tiver n elentos ele retorna um valor menos que n
+//criar condição para determinar se o fread chegou até o final do arquivo
 
 typedef uint8_t BYTE;
 
