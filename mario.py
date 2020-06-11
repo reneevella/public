3fd056b900de8fabaae63cf1f2e5ff252bdc6912
reneevella <reneevella@users.noreply.github.com>
2020-06-11 19:24:25 +0000

    automated commit by check50 [check50=True]

---
+++ mario.py
@@ -0,0 +1,28 @@
+
+height = int(input("How tall will the pyramid be? Please choose between 1 and 8\n"))
+
+col = 0
+hashcount = 1
+
+
+while height < 1 or height > 8:
+
+    height = int(input("Please choose between 1 and 8\n"))
+
+
+col = height
+
+
+while col > 0:
+
+    print(" " * col, end="")
+
+    print("#" * hashcount, end="")
+
+    print("  ", end="")
+
+    print("#" * hashcount, end="")
+
+    print("")
+    hashcount += 1
+    col -= 1
