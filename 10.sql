dff36dbaded85c34dad6b001c32a1f58ad703c85
reneevella <reneevella@users.noreply.github.com>
2020-08-14 20:29:19 +0000

    automated commit by check50 [check50=True]

--- 10.sql
+++ 10.sql
@@ -1,3 +1,3 @@
 SELECT name FROM people
-JOIN movies, ratings, directors ON people.id = directors.person_id AND movies.id = directors.movie_id
+JOIN movies, ratings, directors ON people.id = directors.person_id AND movies.id = directors.movie_id AND ratings.movie_id = movies.id
 WHERE rating >= '9.0';
