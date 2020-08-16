00d105822262264d0d532531494e71b0e13a9822
reneevella <reneevella@users.noreply.github.com>
2020-08-16 14:03:51 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,5 +1,8 @@
-SELECT title FROM people
-JOIN movies, stars ON people.id = stars.person_id AND movies.id = stars.movie_id
-WHERE name = 'Johnny Depp' OR name = 'Helena Bonham Carter'
+SELECT title FROM movies
+JOIN people, stars ON people.id = stars.person_id AND movies.id = stars.movie_id
+WHERE name = 'Johnny Depp' AND title IN ( SELECT title FROM movies
+JOIN people, stars ON people.id = stars.person_id AND movies.id = stars.movie_id
+WHERE name = 'Helena Bonham Carter')
+ORDER BY title;
 
 
